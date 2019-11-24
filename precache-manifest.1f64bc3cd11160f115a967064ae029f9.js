3d3c3a9cc7400b27cc14d6cdfdfb1abec12bdc04
Professor Hall <iprofhall@gmail.com>
2019-11-24 18:45:53 -0500

    Updates

---
+++ precache-manifest.1f64bc3cd11160f115a967064ae029f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0fe24e00897ac9a8187b855d14fab72",
+    "url": "/product-browser/index.html"
+  },
+  {
+    "revision": "9cbf52e098842e7856d0",
+    "url": "/product-browser/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "77d2bf8cd3431848db2e",
+    "url": "/product-browser/static/js/2.2eda3111.chunk.js"
+  },
+  {
+    "revision": "9cbf52e098842e7856d0",
+    "url": "/product-browser/static/js/main.d8e70759.chunk.js"
+  },
+  {
+    "revision": "26296611491e58ca7057",
+    "url": "/product-browser/static/js/runtime-main.d11a696e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/product-browser/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "2a1ae0211e2ef942a8ea2924fb235afe",
+    "url": "/product-browser/static/media/lunch-box.2a1ae021.svg"
+  }
+]);
