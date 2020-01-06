80fb97ccb6b5dcdf6e859aea7e4bbdc77084f576
Phillip Hall <phillip_hall@homedepot.com>
2020-01-06 14:58:52 -0500

    added icons, and content info est

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import styled from "styled-components";
 import colors from "../../Colors";
+import basketPhoto from "../media/foodIcons/harvest.png";
 
 const Contact = () => {
     return (
         <TheContactSection className="container">
             <div className="section">
 
-                <div className="row">
-                    <div className="col s12 center">
-                        <h3><i className="mdi-content-send brown-text"></i></h3>
-                        <h4>Why Go Plant-Based?</h4>
-                        <p className="left-align light">
-                            More info coming soon
-                        </p>
+                <div className="row center ">
+                    <div className="col s12 ">
+                        <h4>
+                            <Icon className="responsive-img" src={basketPhoto}/>
+                            Why Plant-Based?
+                            <Icon className="responsive-img" src={basketPhoto}/>
+
+                        </h4>
+                        <h5 className=" left-align ">
+                            Plant-based diets are often shown to be good for health. Plant-based diets are high in vegetables, whole grains bread, legumes and whole fruits, yet can still contain <b>small amounts of lean meats and reduced-fat dairy products.</b> Studies have shown that eliminating meat and dairy products altogether can be very beneficial to an individuals overall health.
+                        </h5>
+
+                        <h5 className=" left-align ">
+                            Plants are rich sources of many nutrients that are important for good health, including unsaturated fats, vitamins (such as folate), minerals (such as potassium), fibre and protein.
+                        </h5>
+                        <h5 className=" left-align ">
+                            Plant-based diets have been linked to lower risk of obesity and many chronic diseases, such as heart disease, type 2 diabetes, inflammation and cancer. Eating a variety of unprocessed fruits, vegetables, whole grains and legumes is key when it comes to maintaining a healthy, balanced diet.
+                        </h5>
+                        <h5 className=" left-align ">
+                            We do our best to inform you on the nutritional content of all our ingredients. To see our meals along with their nutritional content go to the <a href={"#"}>meals page</a>.
+                        </h5>
                     </div>
                 </div>
 
@@ -28,4 +43,9 @@ const TheContactSection = styled.div`
 color:${colors.secondaryTwo}
 grid-area:contact;
 height: 100%;
+`;
+
+const Icon = styled.img`
+width: 75px;
+margin: 0 20px;
 `;
